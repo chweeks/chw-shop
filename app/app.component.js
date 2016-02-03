2f195cc4b362c4fbb817de3ee8214c837a97d9fe
chweeks <chrisharryweeks@gmail.com>
2016-02-03 14:46:35 +0000

    displays list of products

--- app/app.component.js
+++ app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, PRODUCTS;
     return {
         setters:[
             function (core_1_1) {
@@ -18,17 +18,99 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'CHW Shop';
+                    this.products = PRODUCTS;
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: "<h1>{{title}}</h1>\n             <ul>\n               <li *ngFor='#product of products'>\n                 <h3>Product Details: {{product.name}}</h3>\n                 <p>Price: \u00A3{{product.price}}</p>\n                 <p>Category: {{product.category}}</p>\n                 <p>Quantity In Stock: {{product.quantityInStock}}</p>\n               </li>\n             </ul>\n             "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
             exports_1("AppComponent", AppComponent);
+            PRODUCTS = [
+                { id: 1,
+                    name: 'Almond Toe Court Shoes, Patent Black',
+                    price: 99.00,
+                    category: 'Women’s Footwear',
+                    quantityInStock: 5
+                },
+                { id: 2,
+                    name: 'Suede Shoes, Blue',
+                    price: 42.00,
+                    category: 'Womens Footwear',
+                    quantityInStock: 4
+                },
+                { id: 3,
+                    name: 'Leather Driver Saddle Loafers, Tan',
+                    price: 34.00,
+                    category: 'Men’s Footwear',
+                    quantityInStock: 12,
+                },
+                { id: 4,
+                    name: 'Flip Flops, Red',
+                    price: 19.00,
+                    category: 'Men’s Footwear',
+                    quantityInStock: 6,
+                },
+                { id: 5,
+                    name: 'Flip Flops, Blue',
+                    price: 19.00,
+                    category: 'Men’s Footwear',
+                    quantityInStock: 0
+                },
+                { id: 6,
+                    name: 'Gold Button Cardigan, Black',
+                    price: 167.00,
+                    category: 'Women’s Casualwear',
+                    quantityInStock: 6
+                },
+                { id: 7,
+                    name: 'Cotton Shorts, Medium Red',
+                    price: 30.00,
+                    category: 'Women’s Casualwear',
+                    quantityInStock: 5
+                },
+                { id: 8,
+                    name: 'Fine Stripe Short Sleeve Shirt, Grey',
+                    price: 49.99,
+                    category: 'Men’s Casualwear',
+                    quantityInStock: 9
+                },
+                { id: 9,
+                    name: 'Fine Stripe Short Sleeve Shirt, Green',
+                    price: 39.99,
+                    category: 'Men’s Casualwear',
+                    quantityInStock: 3
+                },
+                { id: 10,
+                    name: 'Sharkskin Waistcoat, Charcoal',
+                    price: 75.00,
+                    category: 'Men’s Formalwear',
+                    quantityInStock: 2
+                },
+                { id: 11,
+                    name: 'Lightweight Patch Pocket Blazer, Deer',
+                    price: 175.50,
+                    category: 'Men’s Formalwear',
+                    quantityInStock: 1
+                },
+                { id: 12,
+                    name: 'Bird Print Dress, Black',
+                    price: 270.00,
+                    category: 'Women’s Formalwear',
+                    quantityInStock: 10
+                },
+                { id: 13,
+                    name: 'Mid Twist Cut-Out Dress, Pink',
+                    price: 540.00,
+                    category: 'Women’s Formalwear',
+                    quantityInStock: 5
+                },
+            ];
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,109 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  quantityInStock: number;
+}
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
-  })
+  template: `<h1>{{title}}</h1>
+             <ul>
+               <li *ngFor='#product of products'>
+                 <h3>Product Details: {{product.name}}</h3>
+                 <p>Price: £{{product.price}}</p>
+                 <p>Category: {{product.category}}</p>
+                 <p>Quantity In Stock: {{product.quantityInStock}}</p>
+               </li>
+             </ul>
+             `
+})
 
-export class AppComponent { }
+export class AppComponent {
+  public title = 'CHW Shop';
+  public products = PRODUCTS;
+}
+
+var PRODUCTS: Product[] = [
+  {id: 1,
+   name: 'Almond Toe Court Shoes, Patent Black',
+   price: 99.00,
+   category: 'Women’s Footwear',
+   quantityInStock: 5
+   },
+  {id: 2,
+   name: 'Suede Shoes, Blue',
+   price: 42.00,
+   category: 'Womens Footwear',
+   quantityInStock: 4
+  },
+  {id: 3,
+   name: 'Leather Driver Saddle Loafers, Tan',
+   price: 34.00,
+   category: 'Men’s Footwear',
+   quantityInStock: 12,
+  },
+  {id: 4,
+   name: 'Flip Flops, Red',
+   price: 19.00,
+   category: 'Men’s Footwear',
+   quantityInStock: 6,
+  },
+  {id: 5,
+   name: 'Flip Flops, Blue',
+   price: 19.00,
+   category: 'Men’s Footwear',
+   quantityInStock: 0
+  },
+  {id: 6,
+   name: 'Gold Button Cardigan, Black',
+   price: 167.00,
+   category: 'Women’s Casualwear',
+   quantityInStock: 6
+  },
+  {id: 7,
+   name: 'Cotton Shorts, Medium Red',
+   price: 30.00,
+   category: 'Women’s Casualwear',
+   quantityInStock: 5
+  },
+  {id: 8,
+   name: 'Fine Stripe Short Sleeve Shirt, Grey',
+   price: 49.99,
+   category: 'Men’s Casualwear',
+   quantityInStock: 9
+  },
+  {id: 9,
+   name: 'Fine Stripe Short Sleeve Shirt, Green',
+   price: 39.99,
+   category: 'Men’s Casualwear',
+   quantityInStock: 3
+  },
+  {id: 10,
+   name: 'Sharkskin Waistcoat, Charcoal',
+   price: 75.00,
+   category: 'Men’s Formalwear',
+   quantityInStock: 2
+  },
+  {id: 11,
+   name: 'Lightweight Patch Pocket Blazer, Deer',
+   price: 175.50,
+   category: 'Men’s Formalwear',
+   quantityInStock: 1
+  },
+  {id: 12,
+   name: 'Bird Print Dress, Black',
+   price: 270.00,
+   category: 'Women’s Formalwear',
+   quantityInStock: 10
+  },
+  {id: 13,
+   name: 'Mid Twist Cut-Out Dress, Pink',
+   price: 540.00,
+   category: 'Women’s Formalwear',
+   quantityInStock: 5
+  },
+]
